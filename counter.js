4ef2eeaf20bbc87cf0ac4c5ee3f0d40389574b29
Tim Hor <timothy.hor@outlook.com>
2020-09-26 15:10:05 +1000

    Add timer controls

--- counter.js
+++ counter.js
@@ -1,19 +1,41 @@
 const timer = document.querySelector('#timer');
+let interval;
 
 const duration = 25 * 60;
+let remainingSeconds;
 
-timer.innerText = formatSeconds(duration);
-
-let remainingSeconds = duration;
-const interval = setInterval(() => {
-  remainingSeconds -= 1;
-  timer.innerText = formatSeconds(remainingSeconds);
-  if (remainingSeconds === 0) {
-    clearInterval(interval);
-  }
-}, 1000);
+resetTimer();
 
 // format duration in seconds as MM:SS
 function formatSeconds(seconds) {
   return new Date(seconds * 1000).toISOString().substring(14, 19);
+}
+
+function toggleTimer() {
+  if (interval) {
+    document.querySelector('#startStop').innerText = 'Start';
+    resetInterval(interval);
+  } else {
+    document.querySelector('#startStop').innerText = 'Stop';
+    interval = setInterval(() => {
+      remainingSeconds -= 1;
+      timer.innerText = formatSeconds(remainingSeconds);
+      if (remainingSeconds === 0) {
+        resetInterval(interval);
+      }
+    }, 1000);
+  }
+}
+
+function resetTimer() {
+  timer.innerText = formatSeconds(duration);
+  remainingSeconds = duration;
+  if (interval) {
+    toggleTimer();
+  }
+}
+
+function resetInterval() {
+  clearInterval(interval);
+  interval = undefined;
 }
